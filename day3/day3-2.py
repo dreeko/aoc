853b4c09d32eae9605cf4c2c54269d3212299e62
dreeko_ <keegan.ott@dreeko.me>
2021-12-08 10:48:32 +0800

    day3 cleanup

--- day3/day3-2.py
+++ day3/day3-2.py
@@ -4,7 +4,8 @@ import math
 
 def gen_sig_set(lines, sig):
     sums = [0] * (len(lines[0]))
-    final_msig = [0] * (len(lines[0]))  # minus newline
+    final_msig = [0] * (len(lines[0]))
+
     for line in lines:
         for iter in range(len(line)):
             sums[iter] += int(line[iter])
@@ -13,28 +14,23 @@ def gen_sig_set(lines, sig):
             final_msig[iter] = 0
         else:
             final_msig[iter] = 1
-        msig_str = '0b' + ''.join([str(x) for x in final_msig])
-        lsig_str = '0b' + ''.join([str(int(not x)) for x in final_msig])
     return [int(not x) for x in final_msig] if sig == 0 else final_msig
+
+
+def fn_filter(lines, sig):
+    line_length = len(lines[0])
+    for iter in range(line_length):
+        print(len(lines))
+        print(len(lines))
+        lines = list(filter(lambda x: int(x[iter]) == int(
+            gen_sig_set(lines, sig)[iter]), lines))
+        if len(lines) <= 1:
+            return lines
 
 
 with open('/home/dreeko/PROJECT/aoc2021/day3/input.txt', 'r') as f:
     lines = [list(x.replace('\n', '')) for x in f.readlines()]
     msig, lsig = [0, 0]
-    line_length = len(lines[0])
-    for iter in range(line_length):
-        print(len(lines))
-        lines = list(filter(lambda x: int(x[iter]) == int(
-            gen_sig_set(lines, 1)[iter]), lines))
-        if(len(lines)) == 1:
-            msig = int('0b'+''.join([str(x) for x in lines[0]]), 2)
-
-    f.seek(0)
-    lines = [list(x.replace('\n', '')) for x in f.readlines()]
-    for iter in range(line_length):
-        print(len(lines))
-        lines = list(filter(lambda x: int(x[iter]) == int(
-            gen_sig_set(lines, 0)[iter]), lines))
-        if(len(lines)) == 1:
-            lsig = int('0b'+''.join([str(x) for x in lines[0]]), 2)
+    msig = int('0b'+''.join([str(x) for x in fn_filter(lines, 1)[0]]), 2)
+    lsig = int('0b'+''.join([str(x) for x in fn_filter(lines, 0)[0]]), 2)
 print(msig, ' ', lsig, msig * lsig)
