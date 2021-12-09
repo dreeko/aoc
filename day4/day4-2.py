a377be94e4055e0cb20e65deac73cd6b2c3561d6
dreeko <keegan.ott@dreeko.me>
2021-12-09 22:54:41 +0800

    day4 p2 complete

--- day4/day4-2.py
+++ day4/day4-2.py
@@ -1,5 +1,6 @@
 import re
 
+
 def fn_build_board():
     boards = []
     with open('/home/dreeko/PERSONAl/aoc2021/day4/input.txt', 'r') as f:
@@ -28,8 +29,8 @@ def fn_search_and_mark(board, draws, min_complete):
     completed_in = 0
     for draw in draws:
         completed_in += 1
-        if completed_in >= min_complete:
-            return 100, board, marked, draws[-1]
+        # if completed_in >= min_complete:
+        #     return 100, board, marked, draws[-1]
         for i in range(0, len(board)):
             for j in range(0, len(board[i])):
                 if board[i][j] == draw:
@@ -41,17 +42,20 @@ def fn_search_and_mark(board, draws, min_complete):
 
 boards, draws = fn_build_board()
 completions = []
-min_complete = 9999
+max_complete = -1
+idx = 0
 for iter in range(0, len(boards)):
-    completed_in, board, marked_mat, last_draw = fn_search_and_mark(boards[iter], draws, min_complete)
+    completed_in, board, marked_mat, last_draw = fn_search_and_mark(
+        boards[iter], draws, max_complete)
     completions.append([completed_in, board, marked_mat, last_draw])
-    min_complete, idx = [completed_in, iter] if completed_in < min_complete else [min_complete, idx]
+    max_complete, idx = [completed_in, iter] if completed_in > max_complete else [
+        max_complete, idx]
 sum_unmarked = 0
 for i in range(len(completions[idx][1])):
     for j in range(len(completions[idx][1][0])):
-        sum_unmarked += int(completions[idx][1][i][j]) if (not completions[idx][2][i][j]) else 0
-
-last_draw = draws[min_complete-1]
+        sum_unmarked += int(completions[idx][1][i][j]
+                            ) if (not completions[idx][2][i][j]) else 0
+last_draw = draws[max_complete-1]
 product = int(sum_unmarked * int(last_draw))
 print(
     f"last draw: {last_draw}\nsum of unmarked: {sum_unmarked}\nproduct: {product}")
