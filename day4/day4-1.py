eaadc64488c7af5ec7126e84bcb6a91355e45378
dreeko <keegan.ott@dreeko.me>
2021-12-09 22:46:47 +0800

    day4 p1 complete

--- day4/day4-1.py
+++ day4/day4-1.py
@@ -1,4 +1,13 @@
+# 93,18,74,26,98,52,94,23,15,2,34,75,13,31,39,76,96,16,84,12,38,27,8,85,86,43,4,79,57,19,40,59,14,21,35,0,90,11,32,17,78,83,54,42,66,82,99,45,55,63,24,5,89,46,80,49,3,48,67,47,50,60,81,51,71,33,72,6,9,30,56,20,77,29,28,69,25,36,91,92,65,22,62,58,64,88,10,7,87,41,44,37,73,70,68,97,61,95,53,1
+# 90 39 48 75 52
+# 78  7 27  8 86
+#  2 69 80 98 66
+# 22 77 11 94 47
+# 93 19 53 34 71
+
+
 import re
+from functools import reduce
 
 
 def fn_build_board():
@@ -29,28 +38,33 @@ def fn_search_and_mark(board, draws, min_complete):
     completed_in = 0
     for draw in draws:
         completed_in += 1
-        #print(completed_in, min_complete)
-        if completed_in > min_complete:
-            return 999
+        if completed_in >= min_complete:
+            return 100, board, marked, draws[-1]
         for i in range(0, len(board)):
             for j in range(0, len(board[i])):
                 if board[i][j] == draw:
-                    print(f"match!: {board[i][j]} {draw}, {i}, {j}")
-                    print(marked[i][j])
                     marked[i][j] = True
-                    # print('-----\n')
-                    # print(marked)
-                    # print('-----\n')
                     if fn_check_complete(marked, i, j):
-                        return completed_in
-    return completed_in
+                        return completed_in, board, marked, draws[completed_in]
+    return 100, board, marked, draws[-1]
 
 
 boards, draws = fn_build_board()
-completions = [len(draws)] * len(boards)
+completions = []
 min_complete = 9999
 for iter in range(0, len(boards)):
-    completions[iter] = fn_search_and_mark(
+    completed_in, board, marked_mat, last_draw = fn_search_and_mark(
         boards[iter], draws, min_complete)
-    print(f"min completion: {min_complete}")
-    min_complete = min(completions)
+    completions.append([completed_in, board, marked_mat, last_draw])
+    min_complete, idx = [completed_in, iter] if completed_in < min_complete else [
+        min_complete, idx]
+sum_unmarked = 0
+for i in range(len(completions[idx][1])):
+    for j in range(len(completions[idx][1][0])):
+        sum_unmarked += int(completions[idx][1][i][j]
+                            ) if (not completions[idx][2][i][j]) else 0
+
+last_draw = draws[min_complete-1]
+product = int(sum_unmarked * int(last_draw))
+print(
+    f"last draw: {last_draw}\nsum of unmarked: {sum_unmarked}\nproduct: {product}")
