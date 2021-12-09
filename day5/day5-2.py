e89a483e873871d3ed9879035a0eed66e98bfd02
dreeko_ <keegan.ott@dreeko.me>
2021-12-09 16:59:35 +0800

    day5 p2

--- day5/day5-2.py
+++ day5/day5-2.py
@@ -1,4 +1,4 @@
-#import numpy as np
+import numpy as np
 np. set_printoptions(threshold=np. inf)
 field = [[0 for i in range(1000)] for j in range(1000)]
 tally = 0
@@ -9,13 +9,14 @@ with open('/home/dreeko/PROJECT/aoc2021/day5/input.txt', 'r') as f:
         x2, y2 = int(end.split(',')[0]), int(end.split(',')[1])
         if not (x1 == x2 or y1 == y2):
             continue
-        for i in range(min(x1, x2), max(x1, x2) + 1):
-            for j in range(min(y1, y2), max(y1, y2) + 1):
-                field[i][j] += 1
-                if field[i][j] == 2:
-                    tally += 1
+        else:
+            for i in range(min(x1, x2), max(x1, x2) + 1):
+                for j in range(min(y1, y2), max(y1, y2) + 1):
+                    field[i][j] += 1
+                    if field[i][j] == 2:
+                        tally += 1
 print(tally)
-# mat = np.matrix(field)
-# with open('outfile.txt', 'wb') as f:
-#     for line in mat:
-#         np.savetxt(f, line, fmt='%d')
+mat = np.matrix(field)
+with open('outfile.txt', 'wb') as f:
+    for line in mat:
+        np.savetxt(f, line, fmt='%d')
